0a7f4b0bacfdda3781e5fe99a746582fe1d7b5b8
Алексей Невротов <elazarsound@gmail.com>
2025-06-17 00:00:49 +0300

    alert

--- game-script.js
+++ game-script.js
@@ -111,7 +111,7 @@ function movePlayer(direction) {
     newX += 1;
   }
   if (mazeData[newY][newX] === 2) {
-    alert('Поздравляем! Ты победил.');
+    alert('Поздравляем! Вы победили.');
   }
   playerPosition = { x: newX, y: newY };
   updatePlayerPosition();
